678859da8a840efc30e8c35a16fa5148cc67893b
Dhanush S <dhanush13244@gmail.com>
2026-04-14 20:53:21 +0530

    completed uc15 -- rework

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,6 +1,19 @@
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
-        UseCase15TrainConsistMgmnt.main(args);
+        System.out.println("====================================");
+        System.out.println("UC15 - Safe Cargo Assignment");
+        System.out.println("====================================");
+        System.out.println();
+
+        GoodsBogie cylindricalBogie = new GoodsBogie("Cylindrical");
+        cylindricalBogie.assignCargo("Petroleum");
+        System.out.println();
+
+        GoodsBogie rectangularBogie = new GoodsBogie("Rectangular");
+        rectangularBogie.assignCargo("Petroleum");
+        System.out.println();
+
+        System.out.println("UC15 runtime handling completed...");
     }
 }
